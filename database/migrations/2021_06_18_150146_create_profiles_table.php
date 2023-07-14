b46270310557450f66948eb127bf65bc7945439e
faranulis <webvizer.com@gmail.com>
2023-07-14 20:32:29 +0700

    final

---
+++ database/migrations/2021_06_18_150146_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
+            $table->integer('nim');
+            $table->string('photo');
+            $table->string('kelas');
+            $table->bigInteger('no_telpon');
+            $table->text('alamat');
+            $table->string('bio');
+            $table->string('jurusan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
